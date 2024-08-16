b373698bc49a16e29ec1f7c0f0a588bc5a87fa4b
rb7a <130118715+rb7a@users.noreply.github.com>
2024-08-16 17:17:02 +0800

    Update server.js

--- server/node/server.js
+++ server/node/server.js
@@ -54,7 +54,7 @@ app.get('/checkout-session', async (req, res) => {
 });
 
 app.post('/create-checkout-session', async (req, res) => {
-  const domainURL = process.env.URL;
+  const domainURL = process.env.DOMAIN;
 
   const { quantity } = req.body;
 
@@ -118,6 +118,8 @@ app.post('/webhook', async (req, res) => {
 
   res.sendStatus(200);
 });
+
+app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
 
 
 function checkEnv() {
